6381c0f5499ede494f511ad7375cdf2c0b99a813
Joshua James Lewis <josh1205_1933@yahoo.com>
2020-09-22 09:12:22 +0200

    Add project files.

---
+++ RectangleServices/Model/AreaRec.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RectangleServices.Model
+{
+    public class AreaRec
+    {
+        public int Length { get; set; }
+        public int Width { get; set; }
+
+        /// <summary>
+        /// Empty constructor
+        /// </summary>
+        public AreaRec()
+        {
+            
+        }
+
+        /// <summary>
+        /// Parameterized Constructor
+        /// </summary>
+        public AreaRec(int length, int width)
+        {
+            Length = length;
+            Width = width;
+        }
+    }
+}
